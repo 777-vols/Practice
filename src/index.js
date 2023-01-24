b85fad08af00d10e575adf39dea5edd8c3c25bb3
Vladislav <samokhvalov.0202@mail.ru>
2023-01-24 19:08:13 +0300

    '1-st'

--- src/index.js
+++ src/index.js
@@ -1,14 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import styled, { createGlobalStyle } from 'styled-components';
+
+const Global = createGlobalStyle`
+*{
+	margin:0;
+	padding:0;
+	box-sizing:border-box;
+}
+body{
+	background:black;
+	display:flex;
+	justify-content: center
+}
+`
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+	<>
+		<Global />
+		<App />
+	</>
 );
 
 // If you want to start measuring performance in your app, pass a function
